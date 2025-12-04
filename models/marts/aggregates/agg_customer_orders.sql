cfe968f4cf6c58758e5d12ea0d3d620f5e74a9ab
Miles Freeborn <mfreeborn@dbtlabs.com>
2025-12-04 08:54:34 -0500

    Add aggregate tables for customer and supplier orders
    - agg_customer_orders: aggregates order metrics by customer
    - agg_supplier_orders: aggregates order metrics by supplier
    - Added documentation and data quality tests (unique, not_null, dbt_expectations)

---
+++ models/marts/aggregates/agg_customer_orders.sql
@@ -0,0 +1,29 @@
+with fct_order_items as (
+    select * from {{ ref('fct_order_items') }}
+),
+
+dim_customers as (
+    select * from {{ ref('dim_customers') }}
+),
+
+final as (
+    select
+        dim_customers.customer_key,
+        dim_customers.name as customer_name,
+        dim_customers.nation,
+        dim_customers.region,
+        dim_customers.market_segment,
+        count(distinct fct_order_items.order_key) as total_orders,
+        count(fct_order_items.order_item_key) as total_order_items,
+        sum(fct_order_items.gross_item_sales_amount) as total_gross_sales,
+        sum(fct_order_items.net_item_sales_amount) as total_net_sales,
+        sum(case when fct_order_items.is_return then 1 else 0 end) as total_returns,
+        avg(fct_order_items.gross_item_sales_amount) as avg_order_item_value
+    from fct_order_items
+    inner join dim_customers
+        on fct_order_items.customer_key = dim_customers.customer_key
+    group by 1, 2, 3, 4, 5
+)
+
+select * from final
+
---
+++ models/marts/aggregates/agg_supplier_orders.sql
@@ -0,0 +1,29 @@
+with fct_order_items as (
+    select * from {{ ref('fct_order_items') }}
+),
+
+dim_suppliers as (
+    select * from {{ ref('dim_suppliers') }}
+),
+
+final as (
+    select
+        dim_suppliers.supplier_key,
+        dim_suppliers.supplier_name,
+        dim_suppliers.nation,
+        dim_suppliers.region,
+        count(distinct fct_order_items.order_key) as total_orders,
+        count(fct_order_items.order_item_key) as total_order_items,
+        sum(fct_order_items.gross_item_sales_amount) as total_gross_sales,
+        sum(fct_order_items.net_item_sales_amount) as total_net_sales,
+        sum(fct_order_items.supplier_cost * fct_order_items.quantity) as total_supplier_cost,
+        sum(fct_order_items.gross_item_sales_amount) - sum(fct_order_items.supplier_cost * fct_order_items.quantity) as gross_margin
+    from fct_order_items
+    inner join dim_suppliers
+        on fct_order_items.supplier_key = dim_suppliers.supplier_key
+    group by 1, 2, 3, 4
+)
+
+select * from final
+
+
